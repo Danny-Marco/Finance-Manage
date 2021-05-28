4828fa3bae5494bc202c7692474247fe3bd23952
Danny Marco <marco.denny7@gmail.com>
2021-05-28 19:01:37 +0600

    Сделал рефакторинг тестов для Account

--- FinanceAccounting/FinanceAccounting.Tests/AccountTests.cs
+++ FinanceAccounting/FinanceAccounting.Tests/AccountTests.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using FinanceAccounting.Controllers;
 using FinanceAccounting.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace FinanceAccounting.Tests
@@ -12,36 +14,54 @@ namespace FinanceAccounting.Tests
         private AccountsController _controller;
         private IActionResult _response;
         private Account _account;
+        private List<Account> _accounts;
         
         [SetUp]
         public void Setup()
         {
+            var dataTest = new DataTest();
             _unitOfWork = new TestUnitOfWork();
             _controller = new AccountsController(_unitOfWork);
+            _accounts = dataTest.CreateAccounts();
         }
 
         [Test]
         public void Should_Return_OkResult()
         {
             _response = _controller.GetAllAccounts();
+            var expectedResult = _accounts;
+            
+            var responseResult = (OkObjectResult) _response;
+            var responseResultValue = responseResult.Value as List<Account>;
+            
+            var expectedCollectionJson = JsonConvert.SerializeObject(expectedResult);
+            var actualCollectionJson = JsonConvert.SerializeObject(responseResultValue);
 
             Assert.That(_response, Is.TypeOf<OkObjectResult>());
+            Assert.AreEqual(expectedCollectionJson, actualCollectionJson);
         }
         
         [Test]
         public void GetAccount_1_ShouldReturn_OkResult()
         {
-            var accountId = 1;
+            const int accountId = 1;
+            var expectedAccount = _accounts.First();
             
             _response = _controller.GetAccount(accountId);
+            var responseResult = (OkObjectResult) _response;
+            var responseResultValue = responseResult.Value as Account;
+            
+            var expectedAccountJson = JsonConvert.SerializeObject(expectedAccount);
+            var actualAccountJson = JsonConvert.SerializeObject(responseResultValue);
             
             Assert.That(_response, Is.InstanceOf<OkObjectResult>());
+            Assert.AreEqual(expectedAccountJson, actualAccountJson);
         }
         
         [Test]
         public void GetAccount_2_ShouldReturn_NotFoundResult()
         {
-            var accountId = 33;
+            var accountId = 404;
             
             _response = _controller.GetAccount(accountId);
             
@@ -53,17 +73,16 @@ namespace FinanceAccounting.Tests
         {
             _account = new Account
             {
-                AccountId = 3,
+                AccountId = 200,
                 CurrentSum = 333,
                 Operations = new List<Operation>()
             };
-
-            var accountQuantity = _unitOfWork.Accounts.Count();
-
             _response = _controller.CreateAccount(_account);
-            
+
+            var IsHasAccount = _unitOfWork.Accounts.GetAll().Any(a => a.AccountId == _account.AccountId);
+
             Assert.That(_response, Is.TypeOf<OkObjectResult>());
-            Assert.That(_unitOfWork.Accounts.Count(), Is.EqualTo(accountQuantity + 1));
+            Assert.That(IsHasAccount, Is.True);
         }
         
         [Test]
@@ -82,24 +101,24 @@ namespace FinanceAccounting.Tests
         public void DeleteAccount_Account_BadRequest()
         {
             var accountId = 3;
-            var accountsQuantity = _unitOfWork.Accounts.Count();
+            var accountsQuantity = _unitOfWork.Accounts.GetAll().Count;
 
             _response = _controller.DeleteAccount(accountId);
             
             Assert.That(_response, Is.TypeOf<NotFoundObjectResult>());
-            Assert.That(_unitOfWork.Accounts.Count(), Is.Not.EqualTo(accountsQuantity - 1));
+            Assert.That(_unitOfWork.Accounts.GetAll().Count, Is.Not.EqualTo(accountsQuantity - 1));
         }
         
         [Test]
         public void DeleteAccount_Account_OkResult()
         {
             var accountId = 2;
-            var accountsQuantity = _unitOfWork.Accounts.Count();
+            var accountsQuantity = _unitOfWork.Accounts.GetAll().Count;
 
             _response = _controller.DeleteAccount(accountId);
             
             Assert.That(_response, Is.TypeOf<OkObjectResult>());
-            Assert.That(_unitOfWork.Accounts.Count(), Is.EqualTo(accountsQuantity - 1));
+            Assert.That(_unitOfWork.Accounts.GetAll().Count, Is.EqualTo(accountsQuantity - 1));
         }
     }
 }
